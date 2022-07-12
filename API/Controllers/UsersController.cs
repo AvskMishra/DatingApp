abafcf821859680d05fc42ed9e8795e6cd46f88a
AvskMishra <abhishek.kumar@kongsbergdigital.com>
2022-07-12 13:56:22 +0530

    API structure added

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _Context;
+        public UsersController(DataContext context)
+        {
+            _Context=context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await   _Context.Users.ToListAsync();
+            return users;
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return  await _Context.Users.FindAsync(id);
+        }
+
+
+    }
+}
